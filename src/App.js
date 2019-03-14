301eec088ed22d5d4b59b900490e41d6373a94c1
Henry Chung <chungthehao@gmail.com>
2019-03-14 10:43:12 +0700

    Background gradient, SEGA LOGO FONT

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList.js';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css';
 
 // const state = {
 //     robots: robots,
@@ -27,7 +28,7 @@ class App extends Component {
 
         return (
             <div className="tc">
-                <h2>RoboFriends</h2>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={ this.onSearchChange } />
                 <CardList robots={ filteredRobots } />
             </div>
